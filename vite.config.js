d5a290a6079e8cd52e34feb66993cb907eb43710
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-03-15 13:32:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  server: {
+    proxy : {
+      "/api" : {
+        target : "https://claimcoupanbackend.onrender.com/",
+        secure : true
+      }
+    }
+  }
 })
